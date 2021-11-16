b00a39767e5ecb581cac7f22a07042e09ca87575
HoGiaHuy2003 <93997565+HoGiaHuy2003@users.noreply.github.com>
2021-11-16 18:26:11 +0700

    fix heroku project

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Heroku</title>
+    </head>
+    <body>
+        <h1>Ho so phat trien abc</h1>
+    </body>
+</html>
